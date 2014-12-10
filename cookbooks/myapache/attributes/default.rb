bc7b8738943bb2a3c975fb42b12eb15c5cd51a3c
johnfitzpatrick <jhnftzptrck@gmail.com>
2014-12-10 11:20:20 +0000

    removed old files

--- cookbooks/myapache/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default['myapache']['greeting'] = "Chefs!"
-default["apache"]["sites"]["chess"] = { "port" => 80 }
-default["apache"]["sites"]["draughts"] = { "port" => 81 }
-
-
--- cookbooks/myapache/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'myapache'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures myapache'
-long_description 'Installs/Configures myapache'
-version          '0.3.0'
-
